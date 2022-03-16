e929bcaa438264ade6326d0a43ce1c9abc13f0d4
Adamgast <92505201+Adamgast@users.noreply.github.com>
2022-03-16 21:28:24 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,158 @@
+function mapAdd() {
+	let tag = document.createElement('script');
+	tag.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDW8ZRJKbCVdneGQhBTUoVVvuCRfsUjiiM&callback=mapInit&v=weekly";
+	let firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+function mapInit(n = 1) {
+	google.maps.Map.prototype.setCenterWithOffset = function (latlng, offsetX, offsetY) {
+		var map = this;
+		var ov = new google.maps.OverlayView();
+		ov.onAdd = function () {
+			var proj = this.getProjection();
+			var aPoint = proj.fromLatLngToContainerPixel(latlng);
+			aPoint.x = aPoint.x + offsetX;
+			aPoint.y = aPoint.y + offsetY;
+			map.panTo(proj.fromContainerPixelToLatLng(aPoint));
+			//map.setCenter(proj.fromContainerPixelToLatLng(aPoint));
+		}
+		ov.draw = function () { };
+		ov.setMap(this);
+	};
+	var markers = new Array();
+	var infowindow = new google.maps.InfoWindow({
+		//pixelOffset: new google.maps.Size(-230,250)
+	});
+	var locations = [
+		[new google.maps.LatLng(53.819055, 27.8813694)],
+		[new google.maps.LatLng(53.700055, 27.5513694)],
+		[new google.maps.LatLng(53.809055, 27.5813694)],
+		[new google.maps.LatLng(53.859055, 27.5013694)],
+	]
+	var options = {
+		zoom: 10,
+		panControl: false,
+		mapTypeControl: false,
+		center: locations[0][0],
+		    styles: [
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#38414e" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }],
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }],
+      },
+    ],
+		scrollwheel: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById('map'), options);
+	var icon = {
+		url: 'img/icons/01.jpg',
+		scaledSize: new google.maps.Size(18, 20),
+		anchor: new google.maps.Point(9, 10)
+	}
+	for (var i = 0; i < locations.length; i++) {
+		var marker = new google.maps.Marker({
+			icon: icon,
+			position: locations[i][0],
+			map: map,
+		});
+		google.maps.event.addListener(marker, 'click', (function (marker, i) {
+			return function () {
+				for (var m = 0; m < markers.length; m++) {
+					markers[m].setIcon(icon);
+				}
+				var cnt = i + 1;
+				//infowindow.setContent(document.querySelector('.events-map__item_' + cnt).innerHTML);
+				//infowindow.open(map, marker);
+				marker.setIcon(icon);
+				map.setCenterWithOffset(marker.getPosition(), 0, 0);
+				setTimeout(function () {
 
+				}, 10);
+			}
+		})(marker, i));
+		markers.push(marker);
+	}
+	if (n) {
+		var nc = n - 1;
+		setTimeout(function () {
+			google.maps.event.trigger(markers[nc], 'click');
+		}, 500);
+	}
+}
+if (document.querySelector('#map')) {
+	mapAdd();
+}
